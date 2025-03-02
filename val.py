ee5787ea1845ad56f64ebee6ab04d2631bd2a1ed
LiuJiaji1999 <494882733@qq.com>
2025-03-02 20:54:31 +0800

    add

--- val.py
+++ val.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 # 验证参数官方详解链接：https://docs.ultralytics.com/modes/val/#usage-examples:~:text=of%20each%20category-,Arguments%20for%20YOLO%20Model%20Validation,-When%20validating%20YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/sourceprivate/weights/best.pt')
+    model = YOLO('/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/improve/sourcecity7/weights/best.pt')
     model.val(data='/home/lenovo/data/liujiaji/powerGit/dayolo/domain/privatepower_to_publicpower.yaml',
               split='val',
               imgsz=800,
@@ -14,7 +14,7 @@ if __name__ == '__main__':
               # rect=False,
               save_json=True, # if you need to cal coco metrice
               project='runs/val/improve',
-              name='sourceprivate',
+              name='sourcecity',
               )
     # city_to_foggycity.yaml sourcecity
     # foggycityscapes.yaml oraclefoggy
