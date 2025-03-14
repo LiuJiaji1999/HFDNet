f99165801b11df4b69fa6a75838058491ae6fefa
LiuJiaji1999 <494882733@qq.com>
2025-03-14 20:26:06 +0800

    add

--- train.py
+++ train.py
@@ -116,6 +116,6 @@ if __name__ == '__main__':
                 cos_lr = True,
                 # project='runs/debug',
                 project='runs/train/improve',
-                name='sourcecity-gmdloss',
+                name='sourcecity-gdm',
                 # mixup = 1.0,
                 )
