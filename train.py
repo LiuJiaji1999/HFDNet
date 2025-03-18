c2f131f44ae8397eac24aaff96b66b8dbcf8408f
LiuJiaji1999 <494882733@qq.com>
2025-03-18 11:11:17 +0800

    add

--- train.py
+++ train.py
@@ -116,6 +116,6 @@ if __name__ == '__main__':
                 cos_lr = True,
                 # project='runs/debug',
                 project='runs/train/improve',
-                name='sourcecity',
+                name='sourcecity-mp-aptdaca',
                 mixup = 1.0,
                 )
