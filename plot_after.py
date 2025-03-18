540f9c780781607dc79aba78700f66fb4e2f59a7
LiuJiaji1999 <494882733@qq.com>
2025-03-18 10:45:02 +0800

    add

--- plot_after.py
+++ plot_after.py
@@ -1,3 +1,3 @@
 from ultralytics.utils.plotting import plot_results
 
-plot_results('/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/improve/sourcecity-mp-gdm/results.csv')
+plot_results('/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/improve/sourcecity-aptdaca/results.csv')
