b9629f0832cb0553288a88346da2c33b3fb49f08
LiuJiaji1999 <494882733@qq.com>
2025-03-19 11:55:16 +0800

    add

--- ultralytics/engine/uda_trainer.py
+++ ultralytics/engine/uda_trainer.py
@@ -630,8 +630,12 @@ class UDABaseTrainer:
                     for layer in [2, 4, 6, 8, 9, 12, 15, 18, 21, 22]:
                         source_feas = self.source_feature_dict[layer]
                         target_feas = self.target_feature_dict[layer]
+                        if type(source_feas) is list and type(target_feas) is list:
+                            for i in range(len(source_feas)):
+                                source_feas = torch.tensor(source_feas[i])
+                                target_feas = torch.tensor(target_feas[i])
                         # # 检查批次大小
-                        min_batch_size = min(source_feas.size(0), target_feas.size(0))
+                        min_batch_size = min(source_feas.shape[0], target_feas.shape[0])
                         source_fea = source_feas[:min_batch_size]
                         target_fea = target_feas[:min_batch_size]
                         if source_fea is not None and target_fea is not None:
@@ -645,28 +649,29 @@ class UDABaseTrainer:
                                     align_corners=False
                                 )
                              # 3.计算源域和目标域的 特定层特征差异损失   ，缩小域间差异
-                            if layer in [2,4,6,8,9]:  # backbone
+                            if layer in [2, 4, 6, 8, 9]:  # backbone
                                 # gram值太小，对结果影响很小
                                 gram_s = gram_matrix(source_fea)
                                 gram_t = gram_matrix(target_fea)
                                 gram_loss = F.mse_loss(gram_s, gram_t).to(self.device)
                                 gram_losses.append(gram_loss)
                             # mean_gram_loss = torch.tensor([sum(gram_losses) / 5])
-                            mean_gram_loss = torch.mean(torch.stack(gram_losses))  # 计算平均值 
                             
                             if layer in [12,15,18,21]:  # neck 
                                 # mmd_linear 在50epoch还行，100epoch就变很小值了！
                                 mmd_loss = torch.tensor(compute_linearmmd_loss(source_fea,target_fea))
                                 mmd_losses.append(mmd_loss)
                             # mean_mmd_loss =  torch.tensor([sum(mmd_losses) / 4]) # 标量没有 detach，除非在 torch.tensor
-                            mean_mmd_loss = torch.mean(torch.stack(mmd_losses))  # 计算平均值
                             
                             if layer in [22]: # head
                                 mse_loss = F.mse_loss(source_fea, target_fea)
                                 mse_losses.append(mse_loss)
                             # mean_mse_loss = torch.tensor([sum(mse_losses)])
-                            mean_mse_loss = torch.mean(torch.stack(mse_losses))
                     
+                    mean_gram_loss = torch.mean(torch.stack(gram_losses))  # 计算平均值
+                    mean_mmd_loss = torch.mean(torch.stack(mmd_losses))  # 计算平均值
+                    mean_mse_loss = torch.mean(torch.stack(mse_losses)) 
+                            
                     # self.loss = self.source_loss + self.args.daca_weight * self.daca_loss
                     self.loss = self.source_loss + self.args.gram_weight * mean_gram_loss + self.args.mmd_weight * mean_mmd_loss + self.args.mse_weight * mean_mse_loss 
                     # self.loss = self.source_loss + self.args.daca_weight * self.daca_loss + self.args.gram_weight * mean_gram_loss + self.args.mmd_weight * mean_mmd_loss + self.args.mse_weight * mean_mse_loss 
