d82527265b426445970be1d69abc3a6a0eec10d5
LiuJiaji1999 <494882733@qq.com>
2025-03-23 20:17:13 +0800

    add

--- train.py
+++ train.py
@@ -103,9 +103,9 @@ if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/models/v8/yolov8m.yaml')
     # model = YOLO('/home/lenovo/data/liujiaji/yolov8/ultralytics-main/runs/train/exp2/weights/last.pt') # 断点续训
     # 域适应会使用 源域 pre-trained weight
-    # model.load('/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/baseline/sourceprivate/weights/best.pt') # loading pretrain weights
+    model.load('/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/baseline/sourcepublic/weights/best.pt') # loading pretrain weights
     # COCO pre-trained weight
-    model.load('yolov8m.pt')
+    # model.load('yolov8m.pt')
     result = model.train(data='/home/lenovo/data/liujiaji/powerGit/dayolo/domain/publicpower_to_privatepower.yaml',
                 cache=False,
                 imgsz=640,
@@ -123,7 +123,6 @@ if __name__ == '__main__':
                 cos_lr = True,
                 # project='runs/debug',
                 project='runs/train/improve',
-                # name='sourceprivate-aptpse-dmm',
-                name = 'sourcepublic-aptpse-dmm'
-                # mixup = 1.0,
+                name = 'sourcepublic-aptpse-dmm',
+                mixup = 1.0,
                 )
