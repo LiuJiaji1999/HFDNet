2c6e52e61959ea1cc0662ef91871c9fad670e263
LiuJiaji1999 <494882733@qq.com>
2026-04-06 19:29:44 +0800

    add

--- val.py
+++ val.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
     # model_path = '/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/improve/sourcevoc-aptpse-dmm/weights/best.pt'
     # model_path = '/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/aqpf/noiou/pu2pr/weights/best.pt'
     # model_path = '/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/aqpf/nodelta/v2c/weights/best.pt'
-    model_path = '/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/aqpf/cdet/v2c/weights/best.pt'
+    model_path = '/home/lenovo/data/liujiaji/yolov8/ultralytics-main-8.2.50/runs/train/aqpf/cdet/pu2pr/weights/best.pt'
    
     model = YOLO(model_path)
-    result = model.val(data='/home/lenovo/data/liujiaji/powerGit/dayolo/domain/voc_to_clipart1k.yaml',
+    result = model.val(data='/home/lenovo/data/liujiaji/powerGit/dayolo/domain/pupower_to_prpower.yaml',
               split='val',
               imgsz=640,
               batch=8,
@@ -26,7 +26,7 @@ if __name__ == '__main__':
               # rect=False,
               save_json=True, # if you need to cal coco metrice
               project='runs/val/apqf',
-              name='v2c-cdet', # nodelta noiou cdet
+              name='pu2pr-cdet', # nodelta noiou cdet
               )
     
     # city_to_foggycity.yaml sourcecity
